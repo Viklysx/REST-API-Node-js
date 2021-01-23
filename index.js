7712ca4db48b80266144302da4e6df9a67dfc18a
Viklysx <vikbelec111@yandex.ru>
2021-01-24 02:48:13 +0300

    feat: configuring the router

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const todoRoutes = require('./routes/todo');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/todo', todoRoutes); // все запросы будем отсылать по адресу api
 app.use((req, res, next) => {
     res.sendFile('/index.html');
 })
---
+++ routes/todo.js
@@ -0,0 +1,20 @@
+const {Router} = require('express');
+const router = Router();
+
+router.get('/', (req, res) => { // получение списка задач
+
+})
+
+router.post('/', (req, res) => { // создание новой задачи
+
+})
+
+router.put('/:id', (req, res) => { // изменение задачи
+
+})
+
+router.delete('/:id', (req, res) => { // удаление задачи
+
+})
+
+module.exports = router;
