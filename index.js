781773936f59e7db6a59a7ec12331e0309885fea
Viklysx <vikbelec111@yandex.ru>
2021-01-24 20:51:12 +0300

    feat: connecting to the database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const todoRoutes = require('./routes/todo');
+const sequelize = require('./utils/database');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,4 +11,15 @@ app.use((req, res, next) => {
     res.sendFile('/index.html');
 })
 
-app.listen(PORT);
+async function start() {
+    try {
+        await sequelize.sync()
+        app.listen(PORT)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start();
+
+// app.listen(PORT);
---
+++ utils/database.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+const DB_NAME = 'node-todo';
+const USER_NAME = 'root';
+const PASSWORD = '1568';
+const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql'
+}); // инициализруем базу данных
+
+module.exports = sequelize;
